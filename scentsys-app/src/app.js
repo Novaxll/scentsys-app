a36cc888e215b9c683d46fa785ac834e5d73358a
Novaxll <zS24003986@estudiantes.uv.mx>
2026-04-20 00:46:19 -0600

    fix: montaje de rutas api en app.js

--- scentsys-app/src/app.js
+++ scentsys-app/src/app.js
@@ -1,18 +1,26 @@
 const express = require('express');
-const cors = require('cors');
+const app = express();
 const path = require('path');
 
+// Importamos las rutas
 const apiRoutes = require('./routes/api.routes');
 const viewsRoutes = require('./routes/views.routes');
 
-const app = express();
-
-app.use(cors());
+// Middlewares
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public'))); // Para tus assets e imágenes
 
-// Mount routes
+// ---> AQUI ESTÁ LA MAGIA <---
+// Le decimos a Express que todo lo que empiece con /api vaya al archivo api.routes.js
+app.use('/api', apiRoutes); 
+
+// Y todo lo demás (las vistas HTML) vaya a views.routes.js
 app.use('/', viewsRoutes);
-app.use('/api', apiRoutes);
+
+// Tu manejador de error 404 (el que está enviando el "No Se Encuentra")
+app.use((req, res) => {
+    res.status(404).send('No Se Encuentra');
+});
 
 module.exports = app;
